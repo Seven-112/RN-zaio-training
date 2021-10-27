35f5ae5f280baadfc661adad987e3eb88ca2a661
myshine112 <bluesatellite777@gmail.com>
2021-10-27 19:06:38 +0200

    Add restaurant image file

---
+++ components/ListRestaurant.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Image, StyleSheet } from "react-native";
+import { ZaioText, ZaioView } from "../plugin";
+
+const ListRestaurant = (props) => {
+    return (
+        <ZaioView elevation={2} row m={10} p={10}>
+            <ZaioView mr={10}>
+                <Image source={props.food.img} style={styles.img} />
+            </ZaioView>
+            <ZaioView middle>
+               <ZaioText bold h1 >Title</ZaioText>
+               <ZaioText grey size={20} normal >subTitle</ZaioText>
+            </ZaioView>
+        </ZaioView>
+    )
+};
+
+const styles = StyleSheet.create({
+  
+    img: {
+        height: 100,
+        width: 100,
+        borderRadius: 10,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 20
+    },
+    subTitle: {
+        fontSize: 20,
+        color: 'grey'
+    },
+});
+
+export default ListRestaurant;
---
+++ screens/ListManagement.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { TextInput } from "react-native";
+import { ZaioView } from "../plugin";
+
+const ListManagement = () => {
+    return (
+        <ZaioView>
+            <TextInput>List Management</TextInput>
+        </ZaioView>
+    )
+};
+
+export default ListManagement;
