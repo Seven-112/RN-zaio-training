7865bd1154f7391cb62c8a9c008ce1f950105fbc
myshine112 <bluesatellite777@gmail.com>
2021-10-28 17:42:15 +0200

    Add food images files

---
+++ contants/Foods.js
@@ -0,0 +1,13 @@
+// Here we are gonna define all the foods as a constants
+
+const Foods = [
+    {
+        id: 1,
+        title: "",
+        details: "",
+        img: "",
+        stars: 1,
+        location: 200,
+        deliveryTime: 20
+    }
+];
