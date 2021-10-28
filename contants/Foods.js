9339c1150fc1443358e3b02ceb4573169d09da84
myshine112 <bluesatellite777@gmail.com>
2021-10-28 17:51:25 +0200

    Create and export a list of food objects

--- contants/Foods.js
+++ contants/Foods.js
@@ -1,13 +1,55 @@
 // Here we are gonna define all the foods as a constants
 
-const Foods = [
+import food1 from '../assets/foods/food1.jpg';
+import food2 from '../assets/foods/food2.jpg';
+import food3 from '../assets/foods/food3.jpg';
+import food4 from '../assets/foods/food4.jpg';
+import food5 from '../assets/foods/food5.jpg';
+
+export default [
     {
         id: 1,
-        title: "",
-        details: "",
-        img: "",
+        title: "Fruits and vegetables",
+        details: "In common language usage, fruit normally means the fleshy seed-associated structures of a plant that are sweet or sour and edible in the raw state",
+        img: food1,
+        stars: 4,
+        location: 210,
+        deliveryTime: 200
+    },
+    {
+        id: 2,
+        title: "Meat, poultry, & seafood",
+        details: "Meat is animal flesh that is eaten as food.[10]: 1  Humans are omnivorous,[11][12][13] and have hunted and killed animals for meat since prehistoric times.",
+        img: food2,
+        stars: 2,
+        location: 920,
+        deliveryTime: 89
+    },
+    {
+        id: 3,
+        title: "Beans",
+        details: "Items served before the main courses of a meal, typically smaller than main dishes, and often meant to be eaten by hand (with minimal use of silverware).",
+        img: food3,
+        stars: 3,
+        location: 450,
+        deliveryTime: 5
+    },
+    {
+        id: 4,
+        title: "Soups",
+        details: "Dipping sauce is a common condiment for many types of food. Dips are used to add flavor or texture to a food.",
+        img: food4,
+        stars: 4,
+        location: 500,
+        deliveryTime: 84
+    },
+    {
+        id: 5,
+        title: "Broth or stock ",
+        details: "Wtew is a combination of solid food ingredients that have been cooked in liquid and served in the resultant gravy. Ingredients in a stew can include any combination of vegetables ",
+        img: food5,
         stars: 1,
-        location: 200,
+        location: 40,
         deliveryTime: 20
     }
 ];
